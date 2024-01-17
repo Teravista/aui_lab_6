c88cdfab03f9f7ea4bc1541f44110862b6e8e4dd
Robert Chudy <48471616+Teravista@users.noreply.github.com>
2024-01-17 13:14:38 +0100

    working perhaps

---
+++ workingperhaps/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'db',
+    'database_port' => '3306',
+    'database_name' => 'BE_188555',
+    'database_user' => 'root',
+    'database_password' => 'student',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'xgRKrpbgneo1VJfcmY95w4ahhsWsnP5tuc2iHqv19Pfqr8svBp1RFxGpeXcUMnVn',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-11-23',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => '3dOgDgE09VvHL2dyIS2DWhyVAb2VQVdo9MhbhE0vHLbiFuRQmqoFTFjPWTGFyLSs',
+    'cookie_iv' => 'gAuS6A3HVSVSF1wJVnp9TU508AjjDVnV',
+    'new_cookie_key' => 'def00000344204f8af8fe7fb24c8dff5981668ef55a485a855b75c092aac462b4c15028857a3e53ff884192e7f9466be3c304393dd04d4f03fc552db11ef25dd083e37da',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArNrrBPGqRTXt3zdfKyKL
+RY8l/2tVUqVN9T6EpAF/i3e5nOqha4xBtsnAp3UGJcJabYyRWdlwFCvFQDnCkA4n
+lDGo6UY1zo/yTWo7vnO7Hne4JI02rhmxcVUwPMW/3ntOm2Blko/NYWndJx18UH9O
+ZNbKLK99qHYf1GmUEJAxH7B15yQbjFUxVMG/hpwW/eE4zz1cgW21GQQs5NWcdvUr
+DlP4OHAwN2XOSIXVQ1jCuC+xSqnngQ9pBlxgUN0fyPH1DLYAv81P4g6Pebs5W/pc
+I9dYXg28XFMagtu8Ao4zENiFVhAn3/bJj6OXNKhqyOqCqizJSo1NXjeE7IE/Ptt6
+SwIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCs2usE8apFNe3f
+N18rIotFjyX/a1VSpU31PoSkAX+Ld7mc6qFrjEG2ycCndQYlwlptjJFZ2XAUK8VA
+OcKQDieUMajpRjXOj/JNaju+c7sed7gkjTauGbFxVTA8xb/ee06bYGWSj81had0n
+HXxQf05k1sosr32odh/UaZQQkDEfsHXnJBuMVTFUwb+GnBb94TjPPVyBbbUZBCzk
+1Zx29SsOU/g4cDA3Zc5IhdVDWMK4L7FKqeeBD2kGXGBQ3R/I8fUMtgC/zU/iDo95
+uzlb+lwj11heDbxcUxqC27wCjjMQ2IVWECff9smPo5c0qGrI6oKqLMlKjU1eN4Ts
+gT8+23pLAgMBAAECggEAKzLY2lb1CS/1nzvs3lBXQw6H3gH6hFmIlppFtuEi+X11
+10fyCHk4cRCRw6fk3enpBIjFSJGlR3064hatT6gh7xDMGGRyDQn/caZzgODWakJc
+NFJQUIleRacXE5EOJvFlD0Kzj8G9f7Nf0Cc3yrxOILhnNWy0ODYxNTofhVYr2LGs
+nz2Xm+JI2ErknBroCIbDlEu8cjYAMa2biYD7CGh66Locj8KNC9bGaf4a/ff9ZbBa
+O6mqapBHhapAXOxsdiYCxbeo9ObRuyaNfwjUddNWNaSvsTFwlIUWUE+qofbv8Y/D
+jyq22tmZxxFg9yzSfm0S7IM2enWtBw4soLi/V4llUQKBgQDsire5JqkRdt0NygJN
+ws3sXY7fs0I7bGDWoMp8UNRGMOM+EtMGxvG4bJH1QbJQkfiXoigWQmCq3n4ZYUZ6
+xG3jifdtNX7+/kIsJuC6vVFqQrzBI1/EXCGrms/9Ut/nfE3FcOpiqb+QA+YfdpM8
+D4WRLSJ4/cFjRIwAEa278JBJ2wKBgQC7Ewk4eipPy7Qt9UiXsxZsSi3Ux/EnCtI5
+bx7BsHS6zP87PTi9lZ/+nuKMlC8cbiJILnG7i+aYnN08vAMQy31HPnL6WOT3Tjhm
+BLx6D6V3cKlYJ73QRPCsAbQHX2cd1J1tBE1ahspTrU+6LZOc6d7WkGFt0qTMyjni
+W0cM+v0UUQKBgQCc0tM/48vPkli4XaftWiwge17QHQ7aSt5hLbVRel1iM+NVMjHK
+mneD6CdUzTulg0GndpDRQSzJ3D0KIp4Ros2gP8ZVF1SdRoEkuJnh9MZXhuuRuRwz
+GUoRddBTiXB9DsKZFmWvCA/DMXKOhZTUCw5o0925DbP7VmTjXtUqdzGSZQKBgGKC
+2LUr58ITY/NbmfmAbgtiWJYXH89ObHugH1Mhp6aCstiwGYQEGbeMVYJvugs/l7/o
+7S00s56j5ADKhNbAQV1V7lgUnaX5Q8u9LL8eJCUsZ5CrhNH6q734O6QyQeu8fZXd
+0Rodpse/mR9SRsw3f4f/ib66UNwila60IEselEyxAoGATtPAMXGACgY09NA68+fp
+uWvmKDc9cKhc10RULO978DRD0jJKpeCjo5PjAw9Yg0ErTjWKd20x795de/1FWf/z
+yNYCjGRDCAshsk5Ah8HEvemnEEWo25gdMzAt7VaDPDpmurfKbXEI+reMOeZqUM3a
+XTBcwf4505bc0Ro4PvrnTPE=
+-----END PRIVATE KEY-----
+',
+  ),
+);
